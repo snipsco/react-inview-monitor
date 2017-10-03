daf13362189c768853f489b3c5d775fb29c13280
erwanledoux <lawanledoux@gmail.com>
2017-10-03 16:13:15 +0200

    removed lodash.throttle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import throttle from 'lodash.throttle'
 
 class InviewMonitor extends Component {
   constructor(props) {
